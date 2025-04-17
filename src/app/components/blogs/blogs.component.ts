6a44eb62b32a0848b9683a898fbf5418da35307e
Sourabh <Mulaysrbh4@gmail.com>
2025-04-17 08:25:04 +0530

    Commit cards

--- src/app/components/blogs/blogs.component.ts
+++ src/app/components/blogs/blogs.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ViewcardComponent } from '../viewcard/viewcard.component';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-blogs',
-  imports: [],
+  imports: [ViewcardComponent, CardComponent],
   templateUrl: './blogs.component.html',
   styleUrl: './blogs.component.scss'
 })
---
+++ src/app/components/viewcard/viewcard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewcard',
+  imports: [],
+  templateUrl: './viewcard.component.html',
+  styleUrl: './viewcard.component.scss'
+})
+export class ViewcardComponent {
+
+}
